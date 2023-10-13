d4856a7a3523d33aaf0717098d3601e31b8b0d81
Alexander Duncombe-J <ad21918@essex.ac.uk>
2023-10-13 10:30:45 +0100

    Basic 3D environment with some simple shapes

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ package src;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point3D;
 import javafx.scene.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -37,13 +40,38 @@ public class Main extends Application {
         box.translateYProperty().set(350);
         box.translateZProperty().set(400);
 
+        camera.setNearClip(0);
+        camera.setFarClip(1000);
+
+        Transform panRight = new Rotate(1, new Point3D(0, 1, 0));
+        Transform panLeft = new Rotate(1, new Point3D(0, 1, 0));
+
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch (event.getCode()){
                 case W:
-                    group.translateZProperty().set(group.getTranslateZ() - 10);
+                    camera.translateZProperty().set(camera.getTranslateZ() + 10);
                     break;
                 case S:
-                    group.translateZProperty().set(group.getTranslateZ() + 10);
+                    camera.translateZProperty().set(camera.getTranslateZ() - 10);
+                    break;
+                case D:
+                    camera.translateXProperty().set(camera.getTranslateX() + 10);
+                    break;
+                case A:
+                    camera.translateXProperty().set(camera.getTranslateX() - 10);
+                    break;
+                case SPACE:
+                    camera.translateYProperty().set(camera.getTranslateY() - 10);
+                    break;
+                case C:
+                    camera.translateYProperty().set(camera.getTranslateY() + 10);
+                    break;
+                case E:
+                    camera.getTransforms().add(panRight);
+                    break;
+                case Q:
+                    camera.getTransforms().add(panLeft);
+                    break;
             }
         });
 
